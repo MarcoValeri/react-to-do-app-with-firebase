f7ccbb851111271ff503cdef9cebe9ffe0ee47e1
MarcoValeri <marcovaleri@hotmail.it>
2023-08-17 10:28:18 +0100

    Add component nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,11 @@
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <nav className="nav">
+            <h1 className="h1">React To Do App With Firebase</h1>
+        </nav>
+    )
+}
+
+export default Nav;
